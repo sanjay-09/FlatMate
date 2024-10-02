a2f5df3350a86738410cbaa9a8679ff180e7a846
Sanjay Singh <63473096+sanjay-09@users.noreply.github.com>
2024-10-03 01:34:46 +0530

    Update index.ts

--- flatemate/db/index.ts
+++ flatemate/db/index.ts
@@ -3,6 +3,7 @@ let isConnected=false;
 export const connect=async()=>{
     await mongoose.connect("mongodb+srv://sanjuwatson0110:fodfEnhrWtV0Nl9r@cluster1.chpu5ky.mongodb.net/flatMate");
     
+    
     isConnected=true;
 
 }
